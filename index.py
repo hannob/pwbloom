62e851707da30c22001aff6c71b4e64603f749e6
Hanno Böck <hanno@gentoo.org>
2021-11-21 12:57:17 +0100

    add web scripts

---
+++ index.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import hashlib
+import cgi
+import bloom
+
+FILTER = "passwords-v6.bloom"
+
+print("Content-type:text/plain")
+print("")
+
+password = cgi.FieldStorage(environ={'REQUEST_METHOD': 'POST'}).getvalue("password")
+if not password:
+    print("error")
+    exit()
+
+value = hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+bfilter = bloom.BloomFilter(FILTER, readonly=True)
+if bfilter.contains(value):
+    print("bad")
+else:
+    print("good")
